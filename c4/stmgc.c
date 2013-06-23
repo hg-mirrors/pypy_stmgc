172beddd0896d048e9e499c07b37df00ab6c36da
Armin Rigo <arigo@tunes.org>
2013-06-23 10:37:56 +0200

    Tweak the compilation.

---
+++ c4/stmgc.c
@@ -0,0 +1,13 @@
+/* This file is optional: it can be used to compile all .c files
+ * into one single big compilation unit.  You must compile either
+ * this, or individually the other .c files in this directory.
+ */
+
+#include "et.c"
+#include "lists.c"
+#include "steal.c"
+#include "nursery.c"
+#include "gcpage.c"
+#include "stmsync.c"
+#include "dbgmem.c"
+#include "fprintcolor.c"
