860d4ef3239e0e43692623363af7bbe1b374beef
Armin Rigo <arigo@tunes.org>
2013-06-27 17:57:01 +0200

    Add a longer version

--- duhton/test/test_gc.py
+++ duhton/test/test_gc.py
@@ -1,22 +1,25 @@
 from support import run
 
 
-def test_long1():
+def test_long1(limit=12):
     got = run("""
         (defun g (n)
-         (if (>= n 12)
+         (if (>= n %d)
              (print n)
            (g (+ n 1))
            (g (+ n 2))))
         (g 0)
-    """)
+    """ % limit)
     pieces = got.splitlines()
     expected = []
     def g(n):
-        if n >= 12:
+        if n >= limit:
             expected.append(str(n))
         else:
             g(n + 1)
             g(n + 2)
     g(0)
     assert pieces == expected
+
+def test_verylong1():
+    test_long1(limit=16)
