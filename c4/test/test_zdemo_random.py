940caa7244781273fa951a3f0f70e9cc38dbf987
Remi Meier <meierrem@student.ethz.ch>
2013-07-02 07:53:39 +0200

    add build and release versions of demo_random to tests

--- c4/test/test_zdemo_random.py
+++ c4/test/test_zdemo_random.py
@@ -2,7 +2,7 @@ import py
 import os
 import subprocess
 
-def test_and_run():
+def test_and_run_debug():
     path = os.path.dirname(__file__)
     path = os.path.dirname(path)
     res = subprocess.call(["make", "debug-demo_random"], cwd=path)
@@ -10,4 +10,19 @@ def test_and_run():
     res = subprocess.call(["./debug-demo_random"], cwd=path)
     assert not res
     
-    
+def test_and_run_build():
+    path = os.path.dirname(__file__)
+    path = os.path.dirname(path)
+    res = subprocess.call(["make", "build-demo_random"], cwd=path)
+    assert not res
+    res = subprocess.call(["./build-demo_random"], cwd=path)
+    assert not res
+
+def test_and_run_release():
+    path = os.path.dirname(__file__)
+    path = os.path.dirname(path)
+    res = subprocess.call(["make", "release-demo_random"], cwd=path)
+    assert not res
+    res = subprocess.call(["./release-demo_random"], cwd=path)
+    assert not res
+
