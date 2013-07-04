cb88819c571e16074c42452e72c772c92106e117
Maciej Fijalkowski <fijall@gmail.com>
2013-07-04 14:19:24 +0200

    Make some use of command-line parameters. Very ad-hoc, but works for now

--- duhton/duhton.c
+++ duhton/duhton.c
@@ -1,20 +1,40 @@
 #include "duhton.h"
 
+#define DEFAULT_NUM_THREADS 4
 
 int main(int argc, char **argv)
 {
-    char *filename;
-    int interactive;
-    if (argc <= 1) {
-        filename = "-";   /* stdin */
-        interactive = 1;
-    }
-    else {
-        filename = argv[1];
-        interactive = 0;
-    }
+    char *filename = NULL;
+    int interactive = 1;
+	int i;
+	int num_threads = DEFAULT_NUM_THREADS;
 
-    Du_Initialize();
+	for (i = 1; i < argc; ++i) {
+		if (strcmp(argv[i], "--help") == 0) {
+			printf("Duhton: a simple lisp-like language with STM support\n\n");
+			printf("Usage: duhton [--help] [--num-threads no] [filename]\n");
+			printf("  --help: this help\n");
+			printf("  --num-threads <number>: number of threads (default 4)\n\n");
+			exit(0);
+		} else if (strcmp(argv[i], "--num-threads") == 0) {
+			if (i == argc - 1) {
+				printf("ERROR: --num-threads requires a parameter\n");
+				exit(1);
+			}
+			num_threads = atoi(argv[i + 1]);
+			i++;
+		} else if (strncmp(argv[i], "--", 2) == 0) {
+			printf("ERROR: unrecognized parameter %s\n", argv[i]);
+		} else {
+			filename = argv[i];
+			interactive = 0;
+		}
+	}
+    if (!filename) {
+        filename = "-";   /* stdin */
+	}
+
+    Du_Initialize(num_threads);
 
     while (1) {
         if (interactive) {
