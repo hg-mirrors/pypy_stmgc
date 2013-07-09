9c4e1dcd04f92f3046d5128c8e065c5bac9d309c
Maciej Fijalkowski <fijall@gmail.com>
2013-07-09 11:33:38 +0200

    implement pair?

--- duhton/test/test_cons.py
+++ duhton/test/test_cons.py
@@ -7,6 +7,10 @@ def test_print_cons():
     assert run("(print (quote (1 2 3)))") == "( 1 2 3 )\n"
     assert run("(print (cons 1 2))") == "( 1 . 2 )\n"
 
+def test_pair():
+    assert run("(print (pair? 1))") == "0\n"
+    assert run("(print (pair? (cons 1 2)))") == "1\n"
+
 def test_car_cdr():
     assert run("(print (car (quote (2 3))))") == "2\n"
     assert run("(print (cdr (quote (2 3))))") == "( 3 )\n"
