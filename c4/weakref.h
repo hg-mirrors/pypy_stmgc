f28b89e101a4b2cd287ef925e4596cebd01503b7
Remi Meier <remi.meier@gmail.com>
2013-09-02 14:44:06 +0200

    more work, still not correct

--- c4/weakref.h
+++ c4/weakref.h
@@ -4,6 +4,7 @@
 #define WEAKREF_PTR(wr, sz)  ((gcptr *)(((char *)(wr)) + (sz) - WORD))
 
 void stm_move_young_weakrefs(struct tx_descriptor *);
+void stm_update_old_weakrefs_lists(void);
 void stm_visit_old_weakrefs(void);
 void stm_clean_old_weakrefs(void);
 
