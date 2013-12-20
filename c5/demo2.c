4e59ad0e7e6316d067d8383e6a23d0e683b56867
Armin Rigo <arigo@tunes.org>
2013-12-20 17:31:40 +0100

    Update demo2.

--- c5/demo2.c
+++ c5/demo2.c
@@ -3,46 +3,48 @@
 #include <assert.h>
 
 
-char *stm_large_malloc(size_t request_size);
-void stm_large_free(char *data);
-void _stm_large_dump(char *data);
+#include "largemalloc.h"
 
-#define dump _stm_large_dump
+#define dump() _stm_large_dump()
+
+
+char buffer[65536];
 
 
 int main()
 {
-    char *d1 = stm_large_malloc(7000);
-    char *start = d1;
-    char *d2 = stm_large_malloc(8000);
-    char *d3 = stm_large_malloc(9000);
+    stm_largemalloc_init(buffer, sizeof(buffer));
 
-    dump(start);
+    char *d1 = stm_large_malloc(7000);
+    char *d2 = stm_large_malloc(8000);
+    /*char *d3 = */ stm_large_malloc(9000);
+
+    dump();
 
     stm_large_free(d1);
     stm_large_free(d2);
 
-    dump(start);
+    dump();
 
     char *d4 = stm_large_malloc(600);
     assert(d4 == d1);
     char *d5 = stm_large_malloc(600);
     assert(d5 == d4 + 616);
 
-    dump(start);
+    dump();
 
     stm_large_free(d5);
 
-    dump(start);
+    dump();
 
     stm_large_malloc(600);
     stm_large_free(d4);
 
-    dump(start);
+    dump();
 
     stm_large_malloc(608);
 
-    dump(start);
+    dump();
 
     return 0;
 }
