726a8c74da5fc81b00aa36edc9a0d90afd53c918
Armin Rigo <arigo@tunes.org>
2014-02-15 21:09:55 +0100

    Mutex and condition variable: the winning combo for writing reasonable code.  I keep rediscovering how they are actually a good idea.  Maybe later we'll figure out that we need more control to avoid spurious wake-ups, but I think that with low numbers of threads it's fine.

--- c7/stm/core.h
+++ c7/stm/core.h
@@ -56,9 +56,23 @@ struct stm_priv_segment_info_s {
     struct list_s *old_objects_to_trace;
     struct list_s *modified_objects;
     struct list_s *creation_markers;
-    uint64_t approximate_start_time;
+    uint64_t start_time;
     uint8_t write_lock_num;
-    uint8_t need_abort;
+    uint8_t safe_point;         /* one of the SP_xxx constants */
+    uint8_t transaction_state;  /* one of the TS_xxx constants */
+};
+
+enum {
+    SP_OUTSIDE=0,
+    SP_RUNNING,
+    SP_SAFE_POINT,
+    SP_SAFE_POINT_CAN_COLLECT,
+};
+enum {
+    TS_NONE=0,
+    TS_REGULAR,
+    TS_INEVITABLE,
+    TS_MUST_ABORT,
 };
 
 static char *stm_object_pages;
@@ -92,10 +106,6 @@ static bool _running_transaction(void);
 
 static inline bool obj_from_same_transaction(object_t *obj) {
     return ((stm_creation_marker_t *)(((uintptr_t)obj) >> 8))->cm != 0;
-}
-
-static inline bool is_inevitable(void) {
-    return STM_SEGMENT->jmpbuf_ptr == NULL;
 }
 
 static void teardown_core(void);
--- c7/stm/sync.h
+++ c7/stm/sync.h
@@ -3,6 +3,13 @@
 static void setup_sync(void);
 static void teardown_sync(void);
 
-/* acquire and release one of the segments for running the given thread */
+/* all synchronization is done via a mutex and condition variable */
+static void mutex_lock(void);
+static void mutex_unlock(void);
+static void cond_wait(void);
+static void cond_broadcast(void);
+
+/* acquire and release one of the segments for running the given thread
+   (must have the mutex acquired!) */
 static void acquire_thread_segment(stm_thread_local_t *tl);
 static void release_thread_segment(stm_thread_local_t *tl);
