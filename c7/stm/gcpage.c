9157058b4234985b528eeb68e42a56f4e44e7e29
Armin Rigo <arigo@tunes.org>
2014-02-17 16:09:20 +0100

    in-progress

--- c7/stm/gcpage.c
+++ c7/stm/gcpage.c
@@ -3,6 +3,18 @@
 #endif
 
 
+/* Outside the nursery, we are taking from the highest addresses
+   complete pages, one at a time, which uniformly contain objects
+   of size "8 * N" for any "2 <= N < GC_N_SMALL_REQUESTS".  We are
+   taking from the lowest addresses large objects, which are
+   guaranteed to be at least 256 bytes long (actually 288),
+   allocated by largemalloc.c.
+*/
+
+#define GC_N_SMALL_REQUESTS    36
+#define GC_MEDIUM_REQUEST      (GC_N_SMALL_REQUESTS * 8)
+
+
 static void setup_gcpage(void)
 {
     /* NB. the very last page is not used, which allows a speed-up in
@@ -11,21 +23,65 @@ static void setup_gcpage(void)
     uintptr_t length = (NB_PAGES - END_NURSERY_PAGE - 1) * 4096UL;
     largemalloc_init_arena(base, length);
 
-    uninitialized_page_start = (stm_char *)(END_NURSERY_PAGE * 4096UL);
-    uninitialized_page_stop = (stm_char *)((NB_PAGES - 1) * 4096UL);
+    uninitialized_page_start = stm_object_pages + END_NURSERY_PAGE * 4096UL;
+    uninitialized_page_stop  = stm_object_pages + (NB_PAGES - 1) * 4096UL;
+
+    assert(GC_MEDIUM_REQUEST >= (1 << 8));
+}
+
+static char *allocate_outside_nursery(uint64_t size)
+{
+    /* not thread-safe!  Use only when holding the mutex */
+    assert(_has_mutex());
+
+    OPT_ASSERT(size >= 16);
+    OPT_ASSERT((size & 7) == 0);
+
+    uint64_t index = size / 8;
+    if (index < GC_N_SMALL_REQUESTS) {
+        assert(index >= 2);
+        // XXX! TEMPORARY!
+        return allocate_outside_nursery(GC_MEDIUM_REQUEST);
+    }
+    else {
+        /* The object is too large to fit inside the uniform pages.
+           Allocate it with largemalloc.c from the lower addresses */
+        char *addr = large_malloc(size);
+
+        if (addr + size > uninitialized_page_start) {
+            uintptr_t pagenum =
+                (uninitialized_page_start - stm_object_pages) / 4096UL;
+            uintptr_t pagecount =
+                (addr + size - uninitialized_page_start) / 4096UL + 20;
+            uintptr_t pagemax =
+                (uninitialized_page_stop - uninitialized_page_start) / 4096UL;
+            if (pagecount > pagemax)
+                pagecount = pagemax;
+            pages_initialize_shared(pagenum, pagecount);
+
+            uninitialized_page_start += pagecount * 4096UL;
+        }
+
+        assert(get_single_creation_marker(
+                   (stm_char *)(addr - stm_object_pages)) == 0);
+        return addr;
+    }
 }
 
 object_t *_stm_allocate_old(ssize_t size_rounded_up)
 {
-    /* XXX not thread-safe! */
-    char *addr = large_malloc(size_rounded_up);
-    stm_char* o = (stm_char *)(addr - stm_object_pages);
+    /* XXX not thread-safe!  and only for tests, don't use when a
+       transaction might be running! */
+    assert(size_rounded_up >= 16);
+    assert((size_rounded_up & 7) == 0);
 
-    if (o + size_rounded_up > uninitialized_page_start) {
+    char *addr = large_malloc(size_rounded_up);
+
+    if (addr + size_rounded_up > uninitialized_page_start) {
         uintptr_t pagenum =
-            ((uint64_t)uninitialized_page_start) / 4096UL;
+            (uninitialized_page_start - stm_object_pages) / 4096UL;
         uintptr_t pagecount =
-            (o + size_rounded_up - uninitialized_page_start) / 4096UL + 20;
+            (addr + size_rounded_up - uninitialized_page_start) / 4096UL + 20;
         uintptr_t pagemax =
             (uninitialized_page_stop - uninitialized_page_start) / 4096UL;
         if (pagecount > pagemax)
@@ -37,6 +93,7 @@ object_t *_stm_allocate_old(ssize_t size_rounded_up)
 
     memset(addr, 0, size_rounded_up);
 
+    stm_char* o = (stm_char *)(addr - stm_object_pages);
     if (CROSS_PAGE_BOUNDARY(o, o + size_rounded_up))
         ((object_t *)o)->stm_flags = GCFLAG_CROSS_PAGE;
 
