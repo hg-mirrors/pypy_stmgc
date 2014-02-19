9fb2794b9e4508e2e6491d6f4fea9c6315544e8c
Armin Rigo <arigo@tunes.org>
2014-02-19 06:43:20 +0100

    Adapt demo2.c.

--- c7/demo2.c
+++ c7/demo2.c
@@ -4,8 +4,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#include "core.h"
-#include "stmsync.h"
+#include "stmgc.h"
 
 #define LIST_LENGTH 6000
 #define BUNCH       400
@@ -20,8 +19,13 @@ struct node_s {
     nodeptr_t next;
 };
 
+__thread stm_thread_local_t stm_thread_local;
 
-size_t stmcb_size(struct object_s *ob)
+#define PUSH_ROOT(p)   (void)0 // XXX...
+#define POP_ROOT(p)    (void)0 // XXX...
+
+
+ssize_t stmcb_size_rounded_up(struct object_s *ob)
 {
     return sizeof(struct node_s);
 }
@@ -34,97 +38,88 @@ void stmcb_trace(struct object_s *obj, void visit(object_t **))
 }
 
 
-nodeptr_t global_chained_list = NULL;
+nodeptr_t global_chained_list;
 
 
-long check_sorted()
+long check_sorted(void)
 {
     nodeptr_t r_n;
     long prev, sum;
-    jmpbufptr_t here;
+    stm_jmpbuf_t here;
 
- back:
-    if (__builtin_setjmp(here) == 0) {
-        stm_start_transaction(&here);
-        
-        stm_read((objptr_t)global_chained_list);
-        r_n = global_chained_list;
-        assert(r_n->value == -1);
-    
-        prev = -1;
-        sum = 0;
-        while (r_n->next) {
-            r_n = r_n->next;
-            stm_read((objptr_t)r_n);
-            sum += r_n->value;
+    STM_START_TRANSACTION(&stm_thread_local, here);
 
-            _stm_start_safe_point(0);
-            _stm_stop_safe_point(0);
-            if (prev >= r_n->value) {
-                stm_stop_transaction();
-                return -1;
-            }
-        
-            prev = r_n->value;
+    stm_read((objptr_t)global_chained_list);
+    r_n = global_chained_list;
+    assert(r_n->value == -1);
+
+    prev = -1;
+    sum = 0;
+    while (r_n->next) {
+        r_n = r_n->next;
+        stm_read((objptr_t)r_n);
+        sum += r_n->value;
+
+        stm_safe_point();
+        if (prev >= r_n->value) {
+            stm_commit_transaction();
+            return -1;
         }
 
-        stm_stop_transaction();
-        return sum;
+        prev = r_n->value;
     }
-    goto back;
+
+    stm_commit_transaction();
+    return sum;
 }
 
 nodeptr_t swap_nodes(nodeptr_t initial)
 {
-    jmpbufptr_t here;
+    stm_jmpbuf_t here;
 
     assert(initial != NULL);
- back:
-    if (__builtin_setjmp(here) == 0) {
-        stm_start_transaction(&here);
-        nodeptr_t prev = initial;
-        stm_read((objptr_t)prev);
-        
-        int i;
-        for (i=0; i<BUNCH; i++) {
-            nodeptr_t current = prev->next;
-            if (current == NULL) {
-                stm_stop_transaction();
-                return NULL;
-            }
-            stm_read((objptr_t)current);
-            nodeptr_t next = current->next;
-            if (next == NULL) {
-                stm_stop_transaction();
-                return NULL;
-            }
-            stm_read((objptr_t)next);
-            
-            if (next->value < current->value) {
-                stm_write((objptr_t)prev);
-                stm_write((objptr_t)current);
-                stm_write((objptr_t)next);
-                
-                prev->next = next;
-                current->next = next->next;
-                next->next = current;
 
-                _stm_start_safe_point(0);
-                _stm_stop_safe_point(0);
-            }
-            prev = current;
+    STM_START_TRANSACTION(&stm_thread_local, here);
+
+    nodeptr_t prev = initial;
+    stm_read((objptr_t)prev);
+
+    int i;
+    for (i=0; i<BUNCH; i++) {
+        nodeptr_t current = prev->next;
+        if (current == NULL) {
+            stm_commit_transaction();
+            return NULL;
         }
+        stm_read((objptr_t)current);
+        nodeptr_t next = current->next;
+        if (next == NULL) {
+            stm_commit_transaction();
+            return NULL;
+        }
+        stm_read((objptr_t)next);
 
-        stm_stop_transaction();
-        return prev;
+        if (next->value < current->value) {
+            stm_write((objptr_t)prev);
+            stm_write((objptr_t)current);
+            stm_write((objptr_t)next);
+
+            prev->next = next;
+            current->next = next->next;
+            next->next = current;
+
+            stm_safe_point();
+        }
+        prev = current;
     }
-    goto back;
+
+    stm_commit_transaction();
+    return prev;
 }
 
 
 
-
-void bubble_run()
+void bubble_run(void)
 {
     nodeptr_t r_current;
 
@@ -136,25 +131,25 @@ void bubble_run()
 
 
 /* initialize list with values in decreasing order */
-void setup_list()
+void setup_list(void)
 {
     int i;
     nodeptr_t w_newnode, w_prev;
 
-    stm_start_transaction(NULL);
+    stm_start_inevitable_transaction(&stm_thread_local);
 
     global_chained_list = (nodeptr_t)stm_allocate(sizeof(struct node_s));
     global_chained_list->value = -1;
     global_chained_list->next = NULL;
-    
-    stm_push_root((objptr_t)global_chained_list);
-    
+
+    PUSH_ROOT(global_chained_list);
+
     w_prev = global_chained_list;
     for (i = 0; i < LIST_LENGTH; i++) {
-        stm_push_root((objptr_t)w_prev);
+        PUSH_ROOT(w_prev);
         w_newnode = (nodeptr_t)stm_allocate(sizeof(struct node_s));
-        
-        w_prev = (nodeptr_t)stm_pop_root();
+
+        POP_ROOT(w_prev);
         w_newnode->value = LIST_LENGTH - i;
         w_newnode->next = NULL;
 
@@ -163,44 +158,31 @@ void setup_list()
         w_prev = w_newnode;
     }
 
-    _stm_minor_collect();       /* hack.. */
-    global_chained_list = (nodeptr_t)stm_pop_root();
-    
-    stm_stop_transaction();
+    //_stm_minor_collect();       /* hack.. */
+    //POP_ROOT(global_chained_list); --- remains in the shadowstack
 
-    
-    
+    stm_commit_transaction();
+
+
     printf("setup ok\n");
 }
 
 
 static sem_t done;
-static sem_t go;
-static sem_t initialized;
 
 
 void *demo2(void *arg)
 {
-    
     int status;
-    if (arg != NULL) {
-        /* we still need to initialize */
-        stm_setup_pthread();
-        sem_post(&initialized);
-        status = sem_wait(&go);
-        assert(status == 0);
-    }
-    
+    stm_register_thread_local(&stm_thread_local);
+
     while (check_sorted() == -1) {
         bubble_run();
     }
 
-    if (arg != NULL) {
-        status = sem_post(&done);
-        assert(status == 0);
-        stm_teardown_pthread();
-    }
-    
+    assert(stm_thread_local.shadowstack == stm_thread_local.shadowstack_base);
+    stm_unregister_thread_local(&stm_thread_local);
+    status = sem_post(&done); assert(status == 0);
     return NULL;
 }
 
@@ -236,32 +218,22 @@ int main(void)
 {
     int status;
 
-    status = sem_init(&initialized, 0, 0);
-    assert(status == 0);
-    status = sem_init(&go, 0, 0);
-    assert(status == 0);
-    
-    stm_setup();
-    stm_setup_pthread();
-    
-    newthread(demo2, (void*)1);
+    status = sem_init(&done, 0, 0); assert(status == 0);
 
-    status = sem_wait(&initialized);
-    assert(status == 0);
+    stm_setup();
+    stm_register_thread_local(&stm_thread_local);
 
     setup_list();
 
-    status = sem_post(&go);
-    assert(status == 0);
-    
-    demo2(NULL);
-    
-    status = sem_wait(&done);
-    assert(status == 0);
-    
+    newthread(demo2, (void*)1);
+    newthread(demo2, (void*)2);
+
+    status = sem_wait(&done); assert(status == 0);
+    status = sem_wait(&done); assert(status == 0);
+
     final_check();
 
-    stm_teardown_pthread();
+    stm_unregister_thread_local(&stm_thread_local);
     stm_teardown();
 
     return 0;
