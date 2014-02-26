6e8b2ae72031df38860ca04a6382310f7639760b
Armin Rigo <arigo@tunes.org>
2014-02-26 16:17:50 +0100

    Move this logic in common.py, otherwise when running only test_list.py the C files are not rebuilt

--- c7/test/common.py
+++ c7/test/common.py
@@ -6,3 +6,22 @@ assert sys.maxint == 9223372036854775807, "requires a 64-bit environment"
 os.environ['CC'] = 'clang'
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# ----------
+
+source_files = [os.path.join(parent_dir, "stmgc.c")]
+all_files = [os.path.join(parent_dir, "stmgc.h"),
+             os.path.join(parent_dir, "stmgc.c")] + [
+    os.path.join(parent_dir, 'stm', _n)
+        for _n in os.listdir(os.path.join(parent_dir, 'stm'))
+            if _n.endswith('.h') or _n.endswith('.c')]
+
+_pycache_ = os.path.join(parent_dir, 'test', '__pycache__')
+if os.path.exists(_pycache_):
+    _fs = [_f for _f in os.listdir(_pycache_) if _f.startswith('_cffi_')]
+    if _fs:
+        _fsmtime = min(os.stat(os.path.join(_pycache_, _f)).st_mtime
+                       for _f in _fs)
+        if any(os.stat(src).st_mtime >= _fsmtime for src in all_files):
+            import shutil
+            shutil.rmtree(_pycache_)
