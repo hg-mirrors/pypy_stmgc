16e635268de7cdf52af569af053c030c2582089b
Armin Rigo <arigo@tunes.org>
2014-03-02 20:03:03 +0100

    Finish prebuilt.c, according to its test (which I forgot to check-in earlier)

--- c7/stm/prebuilt.c
+++ c7/stm/prebuilt.c
@@ -5,23 +5,26 @@
 
 #define GCWORD_PREBUILT_MOVED  ((object_t *) 42)
 
+static struct list_s *prebuilt_objects_to_trace;
 
-object_t *stm_setup_prebuilt(object_t *staticobj_invalid)
+
+static void prebuilt_trace(object_t **pstaticobj_invalid)
 {
-    /* All variable names in "_invalid" here mean that although the
-       type is really "object_t *", it should not actually be accessed
-       via %gs.
+    uintptr_t objaddr = (uintptr_t)*pstaticobj_invalid;
+    struct object_s *obj = (struct object_s *)objaddr;
 
-       If the object was already moved, its first word was set to
+    if (obj == NULL)
+        return;
+
+    /* If the object was already moved, its first word was set to
        GCWORD_PREBUILT_MOVED.  In that case, the forwarding location,
        i.e. where the object moved to, is stored in the second word.
     */
-    uintptr_t objaddr = (uintptr_t)staticobj_invalid;
-    struct object_s *obj = (struct object_s *)objaddr;
     object_t **pforwarded_array = (object_t **)objaddr;
 
     if (pforwarded_array[0] == GCWORD_PREBUILT_MOVED) {
-        return pforwarded_array[1];    /* already moved */
+        *pstaticobj_invalid = pforwarded_array[1];    /* already moved */
+        return;
     }
 
     /* We need to make a copy of this object. */
@@ -32,7 +35,33 @@ object_t *stm_setup_prebuilt(object_t *staticobj_invalid)
     char *realnobj = REAL_ADDRESS(stm_object_pages, nobj);
     memcpy(realnobj, (char *)objaddr, size);
 
-    // XXX REFERENCES HERE
+    /* Mark the original object */
+    pforwarded_array[0] = GCWORD_PREBUILT_MOVED;
+    pforwarded_array[1] = nobj;
 
-    return nobj;
+    /* Done */
+    *pstaticobj_invalid = nobj;
+    LIST_APPEND(prebuilt_objects_to_trace, realnobj);
+}
+
+object_t *stm_setup_prebuilt(object_t *staticobj_invalid)
+{
+    /* All variable names in "_invalid" here mean that although the
+       type is really "object_t *", it should not actually be accessed
+       via %gs.
+    */
+    LIST_CREATE(prebuilt_objects_to_trace);
+
+    object_t *obj = staticobj_invalid;
+    prebuilt_trace(&obj);
+
+    while (!list_is_empty(prebuilt_objects_to_trace)) {
+        struct object_s *realobj1 =
+            (struct object_s *)list_pop_item(prebuilt_objects_to_trace);
+        stmcb_trace(realobj1, &prebuilt_trace);
+    }
+
+    LIST_FREE(prebuilt_objects_to_trace);
+
+    return obj;
 }
