383f40c8586e1f098d89bfd3f031c4558b7a4fbe
Armin Rigo <arigo@tunes.org>
2014-08-12 15:58:35 +0200

    Save and restore slices of the shadowstack in addition to slices of the C stack.

--- c7/stm/rewind_setjmp.h
+++ c7/stm/rewind_setjmp.h
@@ -41,6 +41,7 @@ HEAD-----> | rewind_jmp_buf    |  |        |             |
 
 typedef struct _rewind_jmp_buf {
     char *frame_base;
+    char *shadowstack_base;
     struct _rewind_jmp_buf *prev;
 } rewind_jmp_buf;
 
@@ -48,30 +49,36 @@ typedef struct {
     rewind_jmp_buf *head;
     rewind_jmp_buf *initial_head;
     char *moved_off_base;
+    char *moved_off_ssbase;
     struct _rewind_jmp_moved_s *moved_off;
     void *jmpbuf[5];
     long repeat_count;
 } rewind_jmp_thread;
 
 
-#define rewind_jmp_enterframe(rjthread, rjbuf)   do {   \
-    (rjbuf)->frame_base = __builtin_frame_address(0);   \
-    (rjbuf)->prev = (rjthread)->head;                   \
-    (rjthread)->head = (rjbuf);                         \
+#define rewind_jmp_enterframe(rjthread, rjbuf, ss)   do {  \
+    (rjbuf)->frame_base = __builtin_frame_address(0);      \
+    (rjbuf)->shadowstack_base = (char *)(ss);              \
+    (rjbuf)->prev = (rjthread)->head;                      \
+    (rjthread)->head = (rjbuf);                            \
 } while (0)
 
-#define rewind_jmp_leaveframe(rjthread, rjbuf)   do {   \
-    (rjthread)->head = (rjbuf)->prev;                   \
-    if ((rjbuf)->frame_base == (rjthread)->moved_off_base) \
-        _rewind_jmp_copy_stack_slice(rjthread);         \
+#define rewind_jmp_leaveframe(rjthread, rjbuf, ss)   do {    \
+    assert((rjbuf)->shadowstack_base == (char *)(ss));       \
+    (rjthread)->head = (rjbuf)->prev;                        \
+    if ((rjbuf)->frame_base == (rjthread)->moved_off_base) { \
+        assert((rjthread)->moved_off_ssbase == (char *)(ss));\
+        _rewind_jmp_copy_stack_slice(rjthread);              \
+    }                                                        \
 } while (0)
 
-long rewind_jmp_setjmp(rewind_jmp_thread *rjthread);
+long rewind_jmp_setjmp(rewind_jmp_thread *rjthread, void *ss);
 void rewind_jmp_longjmp(rewind_jmp_thread *rjthread) __attribute__((noreturn));
 
 #define rewind_jmp_forget(rjthread)  do {                               \
     if ((rjthread)->moved_off) _rewind_jmp_free_stack_slices(rjthread); \
     (rjthread)->moved_off_base = 0;                                     \
+    (rjthread)->moved_off_ssbase = 0;                                   \
 } while (0)
 
 void _rewind_jmp_copy_stack_slice(rewind_jmp_thread *);
