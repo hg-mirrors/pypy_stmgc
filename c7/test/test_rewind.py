c44a60dcc26f6d9b294b512dc41b46915459d7d6
Armin Rigo <arigo@tunes.org>
2014-08-12 14:54:57 +0200

    Add a failing test for the preservation of the shadowstack

--- c7/test/test_rewind.py
+++ c7/test/test_rewind.py
@@ -1,17 +1,17 @@
 import os
 
 def run_test(opt):
-    err = os.system("clang -g -O%d -Werror -DRJBUF_CUSTOM_MALLOC -I../stm"
-                    " -o test_rewind_O%d test_rewind.c ../stm/rewind_setjmp.c"
+    err = os.system("clang -g -O%s -Werror -DRJBUF_CUSTOM_MALLOC -I../stm"
+                    " -o test_rewind_O%s test_rewind.c ../stm/rewind_setjmp.c"
                     % (opt, opt))
     if err != 0:
         raise OSError("clang failed on test_rewind.c")
-    for testnum in [1, 2, 3, 4, 5, 6]:
-        print '=== O%d: RUNNING TEST %d ===' % (opt, testnum)
-        err = os.system("./test_rewind_O%d %d" % (opt, testnum))
+    for testnum in [1, 2, 3, 4, 5, 6, "TL1"]:
+        print '=== O%s: RUNNING TEST %s ===' % (opt, testnum)
+        err = os.system("./test_rewind_O%s %s" % (opt, testnum))
         if err != 0:
-            raise OSError("'test_rewind_O%d %d' failed" % (opt, testnum))
-    os.unlink("./test_rewind_O%d" % (opt,))
+            raise OSError("'test_rewind_O%s %s' failed" % (opt, testnum))
+    os.unlink("./test_rewind_O%s" % (opt,))
 
 def test_O0(): run_test(0)
 def test_O1(): run_test(1)
