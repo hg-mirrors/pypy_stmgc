9a8a59822795f902b4174f3e02d1e932f56a6e2a
Remi Meier <remi.meier@inf.ethz.ch>
2014-08-18 13:58:58 +0200

    add all demos to tests

--- c7/test/test_demo.py
+++ c7/test/test_demo.py
@@ -15,6 +15,19 @@ class TestDemo:
 
     def test_shadowstack(self):   self.make_and_run("debug-test_shadowstack")
 
-    def test_demo2_debug(self):   self.make_and_run("debug-demo2")
+    def test_demo_simple_build(self):   self.make_and_run("build-demo_simple")
+    def test_demo_largemalloc_build(self):   self.make_and_run("build-demo_largemalloc")
+
+
+
+    # def test_demo2_debug(self):   self.make_and_run("debug-demo2")
     def test_demo2_build(self):   self.make_and_run("build-demo2")
     def test_demo2_release(self): self.make_and_run("release-demo2")
+
+    # def test_demo_random_debug(self):   self.make_and_run("debug-demo_random")
+    def test_demo_random_build(self):   self.make_and_run("build-demo_random")
+    def test_demo_random_release(self): self.make_and_run("release-demo_random")
+
+    # def test_demo_random2_debug(self):   self.make_and_run("debug-demo_random2")
+    def test_demo_random2_build(self):   self.make_and_run("build-demo_random2")
+    def test_demo_random2_release(self): self.make_and_run("release-demo_random2")
