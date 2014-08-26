1d040ade88e3d5fb80340cffe6786998887560ca
Armin Rigo <arigo@tunes.org>
2014-08-26 15:16:38 +0200

    Port tests back from pypy/duhton about variable-argument && and ||

--- duhton/test/test_int.py
+++ duhton/test/test_int.py
@@ -20,9 +20,11 @@ def test_mul():
     assert evaluate("(* 2 3 7)") == 42
     assert evaluate("(* (+ 5 1) (+ 6 1))") == 42
 
-def test_div():
+def test_div_mod():
     assert evaluate("(/ 11 2)") == 5
     assert evaluate("(/ 29 2 3)") == 4
+    assert evaluate("(% 29 2)") == 1
+    assert evaluate("(% 29 10 3)") == 0
 
 def test_cmp():
     assert evaluate("(<  6 6)") == 0
@@ -47,3 +49,35 @@ def test_cmp():
     assert evaluate("(>= 7 6)") == 1
     #
     assert evaluate("(< (+ 10 2) (+ 4 5))") == 0
+
+def test_and_or():
+    assert evaluate("(&& 1 1 1)") == 1
+    assert evaluate("(&& 1 0 1)") == 0
+    assert evaluate("(&& 0 sdfdsfsd)") == 0
+    assert evaluate("(&& None)") == 0
+    assert evaluate("(&& (quote bla))") == 1
+    assert evaluate("(&& )") == 1
+    
+    assert evaluate("(|| 0 1)") == 1
+    assert evaluate("(|| 0 0 0 1)") == 1
+    assert evaluate("(|| 0 0 0)") == 0
+    assert evaluate("(|| 1 sdfdsfafds)") == 1
+    assert evaluate("(|| None)") == 0
+    assert evaluate("(|| (quote bla))") == 1
+    assert evaluate("(|| )") == 0
+    
+    
+def test_shifts_bitwise():
+    assert evaluate("(<< 1 1)") == 2
+    assert evaluate("(<< 12)") == 12
+    assert evaluate("(<< 1 1 1)") == 4
+    assert evaluate("(<< 0 1)") == 0
+    
+    assert evaluate("(>> 4 1 1)") == 1
+    assert evaluate("(>> 4 3)") == 0
+    assert evaluate("(>> 4)") == 4
+
+    assert evaluate("(^ 1 4)") == 1 ^ 4
+    assert evaluate("(^ 1 4 122)") == 1 ^ 4 ^ 122
+    assert evaluate("(^ 1)") == 1
+    assert evaluate("(^)") == 0
