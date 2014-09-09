0baa436bdfefd9228b541221e27164aff605eccc
Remi Meier <remi.meier@inf.ethz.ch>
2014-09-09 11:19:21 +0200

    add test_rewind

---
+++ c8/test/test_rewind.py
@@ -0,0 +1,19 @@
+import os
+
+def run_test(opt):
+    err = os.system("clang -g -O%s -Werror -DRJBUF_CUSTOM_MALLOC -I../stm"
+                    " -o test_rewind_O%s test_rewind.c ../stm/rewind_setjmp.c"
+                    % (opt, opt))
+    if err != 0:
+        raise OSError("clang failed on test_rewind.c")
+    for testnum in [1, 2, 3, 4, 5, 6, 7, "TL1", "TL2"]:
+        print '=== O%s: RUNNING TEST %s ===' % (opt, testnum)
+        err = os.system("./test_rewind_O%s %s" % (opt, testnum))
+        if err != 0:
+            raise OSError("'test_rewind_O%s %s' failed" % (opt, testnum))
+    os.unlink("./test_rewind_O%s" % (opt,))
+
+def test_O0(): run_test(0)
+def test_O1(): run_test(1)
+def test_O2(): run_test(2)
+def test_O3(): run_test(3)
