25e23347fd491668718f5df90756ad607ae7d8a1
Armin Rigo <arigo@tunes.org>
2014-10-06 16:11:36 +0200

    in-progress

--- c7/stm/finalizer.c
+++ c7/stm/finalizer.c
@@ -14,7 +14,14 @@ static void deal_with_young_objects_with_finalizers(void)
     long i, count = list_count(lst);
     for (i = 0; i < count; i++) {
         object_t* obj = (object_t *)list_item(lst, i);
-        stmcb_light_finalizer(obj);
+        object_t *TLPREFIX *pforwarded_array = (object_t *TLPREFIX *)obj;
+        if (pforwarded_array[0] != GCWORD_MOVED) {
+            /* not moved: the object dies */
+            stmcb_light_finalizer(obj);
+        }
+        else {
+            /*...*/
+        }
     }
     list_clear(lst);
 }
