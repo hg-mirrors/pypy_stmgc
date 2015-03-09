3ec9afbbfd97e555a219d6864bf62faeb4ee0915
Remi Meier <remi.meier@gmail.com>
2015-03-09 13:35:27 +0100

    move some commit log things to separate files

---
+++ c8/stm/commitlog.h
@@ -0,0 +1,40 @@
+
+
+/* Commit Log things */
+struct stm_undo_s {
+    object_t *object;   /* the object that is modified */
+    char *backup;       /* some backup data (a slice of the original obj) */
+    uint64_t slice;     /* location and size of this slice (cannot cross
+                           pages).  The size is in the lower 2 bytes, and
+                           the offset in the remaining 6 bytes. */
+};
+#define SLICE_OFFSET(slice)  ((slice) >> 16)
+#define SLICE_SIZE(slice)    ((int)((slice) & 0xFFFF))
+#define NEW_SLICE(offset, size) (((uint64_t)(offset)) << 16 | (size))
+
+
+
+/* The model is: we have a global chained list, from 'commit_log_root',
+   of 'struct stm_commit_log_entry_s' entries.  Every one is fully
+   read-only apart from the 'next' field.  Every one stands for one
+   commit that occurred.  It lists the old objects that were modified
+   in this commit, and their attached "undo logs" --- that is, the
+   data from 'written[n].backup' is the content of (slices of) the
+   object as they were *before* that commit occurred.
+*/
+#define INEV_RUNNING ((void*)-1)
+struct stm_commit_log_entry_s {
+    struct stm_commit_log_entry_s *volatile next;
+    int segment_num;
+    uint64_t rev_num;
+    size_t written_count;
+    struct stm_undo_s written[];
+};
+static struct stm_commit_log_entry_s commit_log_root;
+
+
+static void free_bk(struct stm_undo_s *undo);
+static struct stm_commit_log_entry_s *malloc_cle(long entries);
+static void free_cle(struct stm_commit_log_entry_s *e);
+
+void _dbg_print_commit_log();
