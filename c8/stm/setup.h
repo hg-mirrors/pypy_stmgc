27b1f9ed7328342a5ab84a7d7737908b5d94d149
Armin Rigo <arigo@tunes.org>
2015-03-12 17:04:45 +0100

    Fix _stm_segfault_expected to start at 1 instead of incrementing it in stm_register_thread_local().  The latter ends up having all the tests run with a large value in _stm_segfault_expected, which is thus never <= 0.

--- c8/stm/setup.h
+++ c8/stm/setup.h
@@ -3,7 +3,7 @@ static void setup_protection_settings(void);
 static pthread_t *_get_cpth(stm_thread_local_t *);
 
 #ifndef NDEBUG
-static __thread long _stm_segfault_expected = 0;
+static __thread long _stm_segfault_expected = 1;
 #define DEBUG_EXPECT_SEGFAULT(v) do {if (v) _stm_segfault_expected++; else _stm_segfault_expected--;} while (0)
 #else
 #define DEBUG_EXPECT_SEGFAULT(v) {}
