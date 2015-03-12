3651d0cd3d1897af3b96e9467cff3e590696fda0
Armin Rigo <arigo@tunes.org>
2015-03-12 17:07:51 +0100

    extra assert

--- c8/stm/setup.h
+++ c8/stm/setup.h
@@ -4,7 +4,7 @@ static pthread_t *_get_cpth(stm_thread_local_t *);
 
 #ifndef NDEBUG
 static __thread long _stm_segfault_expected = 1;
-#define DEBUG_EXPECT_SEGFAULT(v) do {if (v) _stm_segfault_expected++; else _stm_segfault_expected--;} while (0)
+#define DEBUG_EXPECT_SEGFAULT(v) do {if (v) _stm_segfault_expected++; else _stm_segfault_expected--; assert(_stm_segfault_expected <= 1);} while (0)
 #else
 #define DEBUG_EXPECT_SEGFAULT(v) {}
 #endif
