b8a7ba5773e6cefba8d92f53fda721ceb7bdcf6c
Armin Rigo <arigo@tunes.org>
2015-06-19 11:02:11 +0200

    fix: major collections need to trace the added_in_this_transaction items in the correct segment

--- c8/stm/queue.h
+++ c8/stm/queue.h
@@ -1,2 +1,3 @@
 static void queues_deactivate_all(bool at_commit);
-static void collect_active_queues(void);
+static void collect_active_queues(void);           /* minor collections */
+static void mark_visit_from_active_queues(void);   /* major collections */
