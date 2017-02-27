c7154ae03971e59f6c3575b0c9af8809b234257d
Remi Meier <remi.meier@gmail.com>
2017-02-27 17:38:34 +0100

    do a direct NOACCES->ACCESSIBLE if the SIGSEGV is a write operation
    Apparently the kernel can tell us if the SIGSEGV is caused by a write. Then we
    can avoid the NOACCESS->RO->RW dance, saving ~10% of SIGSEGV on test_random.
    
    > strace ~/pypy_dir/pytest.py test_zrandom.py  -vx -k 10 2>&1 |egrep "SIGSEGV"|wc -l

--- c8/stm/signal_handler.h
+++ c8/stm/signal_handler.h
@@ -2,7 +2,7 @@
 static void copy_bk_objs_in_page_from(int from_segnum, uintptr_t pagenum,
                                       bool only_if_not_modified);
 
-static void handle_segfault_in_page(uintptr_t pagenum);
+static void handle_segfault_in_page(uintptr_t pagenum, bool is_write);
 
 
 static void setup_signal_handler(void);
