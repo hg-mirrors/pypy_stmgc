5f61e5f736569b86fa22b1d016559d0b801b3425
Unknown <tobias@masterthesis-vm>
2017-03-06 10:55:56 +0100

    Add macros for timing instrumentation

--- c8/stm/prof.c
+++ c8/stm/prof.c
@@ -39,8 +39,11 @@ static void _stm_profiling_event(stm_thread_local_t *tl,
                                                             buf.extra, EXTRA_LEN_MAX);
             }
         } else if (payload->type == STM_EVENT_PAYLOAD_DURATION) {
-            uint32_t duration = payload->data.duration;
-            buf.extra_length = sprintf(buf.extra, "%u", duration);
+            struct timespec *duration = payload->data.duration;
+            buf.extra_length = sprintf(buf.extra,
+                                        "s%un%u",
+                                        duration->tv_sec,
+                                        duration->tv_nsec);
         }
     }
 
